b0b02d087ca34051ba06950e1ae84f76e63cc9ec
Joshua Smith <joshsmith@nceas.ucsb.edu>
2023-08-01 16:47:43 -0700

    format data script

---
+++ SOFA_format_MBA_data.R
@@ -0,0 +1,227 @@
+#Joshua G. Smith
+#jossmith@mbayaq.org
+#Script initiated March 29, 2023
+
+rm(list=ls())
+
+#load packages
+librarian::shelf(tidyverse, readxl, here, janitor)
+
+#set directories 
+datin <- "/Volumes/seaotterdb$/kelp_recovery/data/foraging_data/raw"
+datout <- "/Volumes/seaotterdb$/kelp_recovery/data/foraging_data/processed"
+
+#get metadata 
+meta_raw <- read_xlsx(file.path(datin, "ForageKey_lookup_table.xlsx"))
+
+#get foraging data
+for_dat <- read_xlsx(file.path(datin, "Forage_data_2016tocurrent.xlsx"))
+dives <- read_xlsx(file.path(datin, "Forage_dives_2016tocurrent.xlsx"))
+index <- read_xlsx(file.path(datin, "Forage_indexi_2016tocurrent.xlsx"))
+
+################################################################################
+#step 1 - tidy data
+
+
+meta_build1 <- meta_raw %>%
+	janitor::clean_names() %>%
+	#Sentence case
+	mutate(category_code = ifelse(data_type == "Prey_vars",
+																str_to_sentence(category_code), category_code),
+				 category_long = str_to_sentence(category_long),
+				 description = str_to_sentence(description))
+
+
+################################################################################
+################################################################################
+#step 2 - process foraging data --- clean up names and check 
+
+for_dat_build1 <- for_dat %>% janitor::clean_names() %>%
+	#correct inconsistencies
+	mutate(qualifier = factor(tolower(qualifier)),
+				 pup_behav = factor(tolower(pup_behav)),
+				 mom_resp = factor(tolower(mom_resp)),
+				 outcome = factor(tolower(outcome)),
+				 prey = factor(prey),
+				 tooltype = factor(tooltype),
+				 pupsh = factor(pupsh),
+				 pup_behav = factor(pup_behav),
+				 stolenfr = factor(stolenfr),
+				 steal = factor(steal),
+				 sizecm = factor(sizecm)
+	)
+
+str(for_dat_build1)
+
+#check for inconsistencies
+unique(for_dat_build1$preynum)       
+unique(for_dat_build1$prey)    
+unique(for_dat_build1$number)    
+unique(for_dat_build1$size)    
+unique(for_dat_build1$qualifier) #make lower
+unique(for_dat_build1$ht)
+unique(for_dat_build1$tooltype)
+unique(for_dat_build1$tool_number)
+unique(for_dat_build1$pupsh)
+unique(for_dat_build1$pupshare_number)
+unique(for_dat_build1$pup_behav) #make lower
+unique(for_dat_build1$mom_resp) #make lower
+unique(for_dat_build1$outcome) #make lower
+unique(for_dat_build1$stolenfr) 
+unique(for_dat_build1$stolenfr_number)
+unique(for_dat_build1$steal)
+unique(for_dat_build1$steal_number)
+unique(for_dat_build1$sizecm)
+unique(for_dat_build1$gulls)
+
+
+################################################################################
+#process dives
+
+dive_build1 <- dives %>% janitor::clean_names() %>%
+	#correct inconsistencies
+	mutate(where = tolower(where),
+				 #fix incorrect sign
+				 long_obs_deg = ifelse(long_obs_deg == 121,-121,long_obs_deg),
+				 #fix kelp type
+				 kelptype = ifelse(kelptype == "xx","x",kelptype),
+				 kelptype = factor(tolower(kelptype)),
+				 success = factor(tolower(success)),
+				 sb_area = factor(toupper(sb_area))
+				 
+	) 
+
+colnames(dive_build1)
+
+str(dive_build1)
+
+#check for inconsistencies
+unique(dive_build1$subbout)       
+unique(dive_build1$where)  
+unique(dive_build1$lat_obs_deg)  
+unique(dive_build1$long_obs_deg)
+unique(dive_build1$long_obs_min)
+unique(dive_build1$lat_obs)
+unique(dive_build1$long_obs)
+unique(dive_build1$long)
+unique(dive_build1$shore)
+unique(dive_build1$depth)
+unique(dive_build1$canopy)
+unique(dive_build1$kelptype)
+unique(dive_build1$divenum)
+unique(dive_build1$dt)
+unique(dive_build1$st)
+unique(dive_build1$success)
+unique(dive_build1$sb_area)
+
+
+
+################################################################################
+#process index
+
+index_build1 <- index %>% janitor::clean_names() %>%
+	mutate(visib = factor(tolower(visib)),
+				 sex = factor(sex),
+				 ageclass = factor(ageclass),
+				 status = factor(status),
+				 pupsz = factor(pupsz),
+				 consort = factor(consort),
+				 obsbeg = factor(obsbeg),
+				 obsend = factor(obsend),
+				 daynight = factor(daynight),
+				 area = factor(area),
+				 sky = factor(sky),
+				 winddir = factor(winddir),
+				 seaopen = factor(seaopen),
+				 seafix = factor(seafix),
+				 swell = factor(swell)
+	)
+
+str(index_build1)
+
+colnames(index_build1)
+
+unique(index_build1$sex)  
+unique(index_build1$ageclass)  
+unique(index_build1$status)  
+unique(index_build1$pupsz)  
+unique(index_build1$ageweeks)  
+unique(index_build1$agequal)  
+unique(index_build1$consort)  
+unique(index_build1$gen_locat)  
+unique(index_build1$atos)  
+unique(index_build1$area)  
+unique(index_build1$fixtype) 
+unique(index_build1$sky)  
+unique(index_build1$rain)  
+unique(index_build1$windspeed)  
+unique(index_build1$winddir)  
+unique(index_build1$temperature)  
+unique(index_build1$seaopen)  
+unique(index_build1$seafix)  
+unique(index_build1$swell)  
+unique(index_build1$visib)  
+
+
+
+################################################################################
+#merge data
+
+forage_join <- for_dat_build1 %>%
+	#filter(steal == "No")%>% 
+	#select variables of interest
+#	dplyr::select(foragdiv_id, foragdata_id, preynum, prey, number,
+#								size, qualifier)%>%
+	mutate_if(is.character, str_trim)
+
+dive_join <- dive_build1 %>%
+#	dplyr::select(foragdiv_id, bout, subbout, lat, long, canopy,
+#								kelptype, divenum, dt, success) %>%
+	mutate_if(is.character, str_trim)
+	#filter successful dives only
+	#filter(success == "y")
+
+index_join <- index_build1 %>%
+	dplyr::select(bout, date, otterno, sex, ageclass, status)%>%
+	mutate_if(is.character, str_trim)
+
+
+#merge forage data and bout info
+data_build1 <- left_join(dive_join, forage_join, by="foragdiv_id")
+anti_build <- anti_join(dive_join, forage_join, by="foragdiv_id")
+
+
+#merge with index
+data_build2 <- left_join(data_build1, index_join, by="bout")
+anti_build2 <- anti_join(data_build1, index_join, by="bout")
+
+
+################################################################################
+#format for SOFA
+
+data_build3 <- data_build2 %>%
+	#set user-defined fields
+	mutate(Region = "CALI", 
+				 Area = "MONT",
+				 Site = "MONT",
+				 Period = "2016-2020",
+				 Date = format(date, "%m/%d/%Y"),
+				 Season = case_when(
+				 	month(date) %in% 5:10 ~ "summer",
+				 	TRUE ~ "winter"
+				 )
+				 )%>%
+	dplyr::select(Region, Area, Site, Period, Date, Season, foragdiv_id, foragdata_id, bout, subbout,
+								lat, long, otterno, everything()
+	)
+
+
+#write.csv(data_build3, file.path(datout, "foraging_data_2016_2023.csv"), row.names=FALSE)
+
+
+
+
+
+
+
+
